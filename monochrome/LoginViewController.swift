458e1224f060c7e131dc4cd6fb9b875f101b3df1
Siew Mai Chan <chansiewmai@gmail.com>
2015-12-06 01:53:41 +0800

    added app icon, splash and login view

---
+++ monochrome/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  monochrome
+//
+//  Created by Siew Mai Chan on 06/12/2015.
+//  Copyright © 2015 Siew Mai Chan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+       // performSegueWithIdentifier("CreateProfile", sender: self)
+        
+    }
+}
---
+++ monochrome/MonoButton.swift
@@ -0,0 +1,20 @@
+//
+//  MonoButton.swift
+//  monochrome
+//
+//  Created by Siew Mai Chan on 06/12/2015.
+//  Copyright © 2015 Siew Mai Chan. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class MonoButton: UIButton {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 2.0
+        layer.shadowColor = UIColor(red: COLOR_SHADOW, green: COLOR_SHADOW, blue: COLOR_SHADOW, alpha: 0.5).CGColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSizeMake(0.0, 2.0)
+    }
+}
