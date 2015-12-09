01524c90ba94695e36fe350cddf047d2e72f75c3
Siew Mai Chan <chansiewmai@gmail.com>
2015-12-10 05:26:06 +0800

    clean

--- monochrome/Config.swift
+++ monochrome/Config.swift
@@ -6,13 +6,3 @@
 //  Copyright © 2015 Siew Mai Chan. All rights reserved.
 //
 
-import AmazonS3RequestManager
-
-// Firebase
-let FIREBASE_URL = "https://mai-monochrome.firebaseio.com"
-
-// Amoazon S3
-let BUCKET_NAME = "monochrome"
-let ACCESS_KEY = "AKIAJGBIAVQNBDWM3PAQ"
-let SECRET_KEY = "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"
-let REGION = AmazonS3Region.USStandard
